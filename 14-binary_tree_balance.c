5c45c9c52abeb7c2db30b4187b2ae7bcf64ef679
Mariam <mariam.yasser772000@gmail.com>
2024-03-27 17:29:41 -0700

    binary trees

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -6,7 +6,7 @@
  * Return: the height or 0
  */
 
-size_t binary_tree_height_b(const binary_tree_t *tree)
+size_t binary_tree_height_balance(const binary_tree_t *tree)
 {
 	size_t Left = 0;
 	size_t Right = 0;
@@ -14,8 +14,8 @@ size_t binary_tree_height_b(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	Left = tree->left ? 1 + binary_tree_height_b(tree->left) : 1;
-	Right = tree->right ? 1 + binary_tree_height_b(tree->right) : 1;
+	Left = tree->left ? 1 + binary_tree_height_balance(tree->left) : 1;
+	Right = tree->right ? 1 + binary_tree_height_balance(tree->right) : 1;
 
 	return ((Left > Right) ? Left : Right);
 }
@@ -28,14 +28,12 @@ size_t binary_tree_height_b(const binary_tree_t *tree)
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	size_t Left_h, Right_h;
+	int Right_h = 0, Left_h = 0;
 
-	if (!tree)
-		return (0);
-	if (tree->left)
-		Left_h = binary_tree_height(tree->left);
-	if (tree->right)
-		Right_h = binary_tree_height(tree->right);
-
-	return ((int)(Left_h - Right_h));
+	if (tree)
+	{
+		Left_h = ((int)binary_tree_height_b(tree->left));
+		Right_h = ((int)binary_tree_height_b(tree->right));
+	}
+	return (Left_h - Right_h);
 }
